930818e722fa26152174d6ab24cc29e0bb5b2af2
Usuario <Usuario@CPD02-M16>
2024-09-24 11:59:01 -0300

    Adicionar arquivos de projeto.

---
+++ 231402_221225/Banco.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Linq.Expressions;
+
+namespace _231402_221225
+{
+    public class Banco
+    {
+        public static MySqlConnection Conexao;
+
+        public static MySqlCommand Comando;
+
+        public static MySqlDataAdapter Adaptador;
+
+        public static DataTable datTabela;
+
+        public static void AbrirConexao()
+        {
+
+            try
+            {
+                Conexao = new MySqlConnection("Server=localhost;port=3307;uid=root;pwd=etecjau");
+
+                Conexao.Open();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void FecharBanco()
+        {
+            try
+            {
+                Conexao.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void CriarBanco()
+        {
+            try
+            {
+                AbrirConexao();
+
+                Comando = new MySqlCommand("CREATE DATABASE IF NOT EXISTIS vendas; USE vendas", Conexao);
+
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS Cidade " +
+                                           "(id integer auto_increment primary key, " +
+                                           "nome char(40), " +
+                                           "uf char (02))", Conexao);
+                Comando.ExecuteNonQuery();
+
+                FecharBanco();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+            Banco.CriarBanco();
+        }
+    }
+}
